34f75172609170d009705d5f543f265fac125f60
pullipulli <andreapullia@rocketmail.com>
2025-07-23 00:17:20 +0200

    Mouse Locking when exiting inventory

--- Assets/Scripts/InputHandler.cs
+++ Assets/Scripts/InputHandler.cs
@@ -1,9 +1,22 @@
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(PlayerInput))]
 public class InputHandler : MonoBehaviour
 {
+    public InputHandler Instance { get; private set; }
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+        else Destroy(this);
+    }
+
+    private bool _isInventoryFocused = false;
     void OnScrollWheel(InputValue value)
     {
         float scrollValue = value.Get<Vector2>().y;
@@ -13,6 +26,37 @@ public class InputHandler : MonoBehaviour
 
     void OnInteract(InputValue value) 
     {
+        if (_isInventoryFocused) return;
+
         Inventory.Instance.UseItem();
+    }
+
+    void OnMove()
+    {
+        if (_isInventoryFocused) return;
+    }
+
+    void OnLook()
+    {
+        if (_isInventoryFocused) return;
+    }
+
+    void OnDrop()
+    {
+
+    }
+
+    void OnInventory()
+    {
+        if (_isInventoryFocused)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            _isInventoryFocused = false;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.None;
+            _isInventoryFocused = true;
+        }
     }
 }

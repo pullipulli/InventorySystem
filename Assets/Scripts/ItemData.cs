1b111a649ca2357e42762378ae92ae15f33f49d6
pullipulli <andreapullia@rocketmail.com>
2025-07-24 18:16:05 +0200

    Removed stacking idea

--- Assets/Scripts/ItemData.cs
+++ Assets/Scripts/ItemData.cs
@@ -6,12 +6,10 @@ public class ItemData : ScriptableObject
     [SerializeField] private string _itemName;
     [SerializeField] private Sprite _icon;
     [SerializeField, TextArea] private string _tooltip;
-    [SerializeField, Min(0)] private int _maxStackSize;
     [SerializeField, Min(0)] private float _droppedRotationSpeed = 0f;
 
     public string ItemName { get => _itemName; }
     public Sprite Icon { get => _icon; }
     public string Tooltip { get => _tooltip; }
-    public int MaxStackSize { get => _maxStackSize; }
     public float DroppedRotationSpeed { get => _droppedRotationSpeed; }
 }
